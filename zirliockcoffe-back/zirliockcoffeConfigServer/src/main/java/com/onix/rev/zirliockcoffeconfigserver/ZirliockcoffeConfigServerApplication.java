da3a649552931c8e4059610db3a0c2d760457e28
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-07-02 00:12:47 -0500

    Se agregan prueas para el servidor de configuraciones

---
+++ zirliockcoffe-back/zirliockcoffeConfigServer/src/main/java/com/onix/rev/zirliockcoffeconfigserver/ZirliockcoffeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.onix.rev.zirliockcoffeconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZirliockcoffeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZirliockcoffeConfigServerApplication.class, args);
+	}
+
+}
---
+++ zirliockcoffe-back/zirliockcoffeConfigServer/src/test/java/com/onix/rev/zirliockcoffeconfigserver/ZirliockcoffeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onix.rev.zirliockcoffeconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZirliockcoffeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
