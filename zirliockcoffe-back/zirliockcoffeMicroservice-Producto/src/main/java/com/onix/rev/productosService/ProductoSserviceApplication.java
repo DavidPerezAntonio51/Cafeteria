01ef5fe3def3b9cf0cb7b675737ff42c726833fc
Dragonibax <sabino.snm@gmail.com>
2022-07-01 20:55:57 -0500

    Carpetas front y back
    Happy hacking!

---
+++ zirliockcoffe-back/zirliockcoffeMicroservice-Producto/src/main/java/com/onix/rev/productosService/ProductoSserviceApplication.java
@@ -0,0 +1,13 @@
+package com.onix.rev.productosService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoSserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoSserviceApplication.class, args);
+	}
+
+}
---
+++ zirliockcoffe-back/zirliockcoffeMicroservice-Producto/src/test/java/com/onix/rev/productosService/ProductoSserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onix.rev.productosService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoSserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
