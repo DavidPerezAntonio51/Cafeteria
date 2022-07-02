b7eee61db5d1bdc8a51ca8d642216e3be1a02635
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-07-02 00:22:20 -0500

    Se cambian las extensiones de archivos

--- zirliockcoffe-back/zirliockcoffeConfigServer/src/main/java/com/onix/rev/zirliockcoffeconfigserver/ZirliockcoffeConfigServerApplication.java
+++ zirliockcoffe-back/zirliockcoffeConfigServer/src/main/java/com/onix/rev/zirliockcoffeconfigserver/ZirliockcoffeConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.onix.rev.zirliockcoffeconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ZirliockcoffeConfigServerApplication {
 
 	public static void main(String[] args) {
