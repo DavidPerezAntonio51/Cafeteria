68c9070d6fc7146ae79137dd1e68ddbf3a861dc0
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-07-01 21:11:49 -0500

    Se agrega la plantilla del microservicio personal

---
+++ zirliockcoffe-back/zirliockcoffeMicroservice-Personal/src/main/java/com/onix/rev/personalservice/ZirliockcoffeMicroservicePersonalApplication.java
@@ -0,0 +1,13 @@
+package com.onix.rev.personalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZirliockcoffeMicroservicePersonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZirliockcoffeMicroservicePersonalApplication.class, args);
+    }
+
+}
