0e852362357f1cab5a2ebdb623adb84094efc79a
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-07-06 18:39:43 -0500

    Se Implementa Eureka

---
+++ zirliockcoffe-back/EurekaServer/src/test/java/com/onix/rev/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onix.rev.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- zirliockcoffe-back/zirliockcoffeMicroservice-Personal/src/main/java/com/onix/rev/personalservice/ZirliockcoffeMicroservicePersonalApplication.java
+++ zirliockcoffe-back/zirliockcoffeMicroservice-Personal/src/main/java/com/onix/rev/personalservice/ZirliockcoffeMicroservicePersonalApplication.java
@@ -2,9 +2,11 @@ package com.onix.rev.personalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class ZirliockcoffeMicroservicePersonalApplication {
 
     public static void main(String[] args) {
