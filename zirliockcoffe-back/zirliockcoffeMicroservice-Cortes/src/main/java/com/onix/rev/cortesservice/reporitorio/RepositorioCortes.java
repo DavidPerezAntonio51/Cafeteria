e2a2b0b7f7aaca66674a028d554cc2e1a45814bf
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-07-09 17:53:10 -0500

    se implementa una gateway simple

--- zirliockcoffe-back/zirliockcoffeMicroservice-Cortes/src/main/java/com/onix/rev/cortesservice/reporitorio/RepositorioCortes.java
+++ zirliockcoffe-back/zirliockcoffeMicroservice-Cortes/src/main/java/com/onix/rev/cortesservice/reporitorio/RepositorioCortes.java
@@ -1,6 +1,7 @@
 package com.onix.rev.cortesservice.reporitorio;
 
+import com.onix.rev.cortesservice.DocumentosDB.Corte;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RepositorioCortes extends MongoRepository<Object, String> {
+public interface RepositorioCortes extends MongoRepository<Corte, String> {
 }
